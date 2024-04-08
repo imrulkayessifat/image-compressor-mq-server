5842488bbce799f123f8a40ef0bad7aeeb4e80b5
imrulkayessifat <kayesimrul224@gmail.com>
2024-04-08 16:58:24 +0600

    fixing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { PrismaClient } from "@prisma/client";
+
 const amqp = require('amqplib/callback_api');
 
 const app = express();
@@ -10,6 +12,7 @@ app.use(express.json())
 app.use(cors());
 app.options('*', cors());
 
+const db = new PrismaClient();
 
 app.get("/", (req, res) => {
     res.json({ message: "demo response" }).status(200)
@@ -19,7 +22,13 @@ app.post("/compress-image", async (req, res) => {
     try {
         const compressData = req.body;
 
-        const { id } = compressData;
+        const { id, productid, url } = compressData;
+
+        await db.image.update({
+            where: { id: id },
+            data: { status: 'ONGOING' },
+        });
+
         amqp.connect('amqp://localhost', function (error0: any, connection: { createChannel: (arg0: (error1: any, channel: any) => void) => void; close: () => void; }) {
             if (error0) {
                 throw error0;
@@ -29,13 +38,13 @@ app.post("/compress-image", async (req, res) => {
                     throw error1;
                 }
 
-                const queue = 'image_queue';
+                const queue = 'shopify_to_compressor';
 
                 channel.assertQueue(queue, {
                     durable: false
                 });
-
-                channel.sendToQueue(queue, Buffer.from(id));
+                const data = JSON.stringify({ id, productid, url });
+                channel.sendToQueue(queue, Buffer.from(data));
                 console.log(" [x] Sent %s", id);
             });
             setTimeout(function () {
@@ -44,6 +53,42 @@ app.post("/compress-image", async (req, res) => {
         });
 
         res.json({ status: 'Image compression started' });
+
+    } catch (e) {
+        res.status(500).json({ error: 'An error occurred while compressing image.' });
+    }
+})
+
+app.post("/upload-image", async (req, res) => {
+    try {
+        const compressData = req.body;
+
+        const { id, productid, compressedBuffer } = compressData;
+
+        amqp.connect('amqp://localhost', function (error0: any, connection: { createChannel: (arg0: (error1: any, channel: any) => void) => void; close: () => void; }) {
+            if (error0) {
+                throw error0;
+            }
+            connection.createChannel(function (error1, channel) {
+                if (error1) {
+                    throw error1;
+                }
+
+                const queue = 'compressor_to_uploader';
+
+                channel.assertQueue(queue, {
+                    durable: false
+                });
+                const data = JSON.stringify({ id, productid, compressedBuffer });
+                channel.sendToQueue(queue, Buffer.from(data));
+                console.log(" [x] Sent %s", id);
+            });
+            setTimeout(function () {
+                connection.close();
+            }, 500);
+        });
+
+        res.json({ status: 'Image Uploading started' });
 
     } catch (e) {
         res.status(500).json({ error: 'An error occurred while compressing image.' });
