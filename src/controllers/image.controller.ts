c09b4c8b971cb1694f9353e26b499a1485c5c5d0
imrulkayessifat <kayesimrul224@gmail.com>
2024-06-11 00:15:31 +0600

    failed to connect

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -366,7 +366,7 @@ export const uploadImage = async (req: Request, res: Response): Promise<void> =>
 
         console.log("upload Image",storeName)
 
-        amqp.connect('amqps://localhost', (error0, connection) => {
+        amqp.connect('amqp://localhost', (error0, connection) => {
             if (error0) {
                 throw error0;
             }
