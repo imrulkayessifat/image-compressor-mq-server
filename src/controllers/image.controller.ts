1ab06808db2271b17b317cb80bfd9212108f0af5
imrulkayessifat <kayesimrul224@gmail.com>
2024-06-11 11:46:40 +0600

    failed to connect

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -1,6 +1,8 @@
 import amqp from 'amqplib/callback_api';
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import axios from 'axios';
+import { uploadFile } from '@uploadcare/upload-client'
 
 const db = new PrismaClient();
 
@@ -364,30 +366,184 @@ export const uploadImage = async (req: Request, res: Response): Promise<void> =>
         const compressData = req.body;
         const { id, productid, compressedBuffer, storeName } = compressData;
 
-        console.log("upload Image",storeName)
+        console.log("upload Image", storeName)
 
-        amqp.connect('amqp://localhost', (error0, connection) => {
-            if (error0) {
-                throw error0;
+        const base64Image = Buffer.from(compressedBuffer).toString('base64');
+
+        const singleImageData = await db.image.findFirst({
+            where: {
+                id: id
             }
-            connection.createChannel((error1, channel) => {
-                if (error1) {
-                    throw error1;
+        })
+
+        console.log(singleImageData)
+
+        const singleProductData = await db.product.findFirst({
+            where: {
+                id: productid
+            }
+        })
+
+        if (productid !== '1') {
+            const image = {
+                alt: `${singleImageData.name.split('.').slice(0, -1).join('.')}-${id}C.${singleImageData.name.split('.').pop()}`,
+                product_id: productid,
+                attachment: base64Image,
+            };
+
+            const accessTokenResponse = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/oauth/access_token`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    'client_id': `${process.env.SHOPIFY_CLIENT_ID}`,
+                    'client_secret': `${process.env.SHOPIFY_CLIENT_SECRET}`,
+                    'grant_type': 'client_credentials'
+                })
+            })
+
+            const accessToken = await accessTokenResponse.json();
+
+            const getImageData = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/api/2024-01/products/${productid}/images/${id}.json`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Shopify-Access-Token': `${accessToken.access_token}`
+                },
+            })
+
+            const getImageDataRes = await getImageData.json()
+
+            const response = await axios.get(getImageDataRes.image.src, { responseType: 'arraybuffer' });
+            const buffer = Buffer.from(response.data, 'binary');
+            const base64ImageForBackup = Buffer.from(buffer).toString('base64');
+
+
+            const deleteImage = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/api/2024-01/products/${productid}/images/${id}.json`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Shopify-Access-Token': `${accessToken.access_token}`
+                },
+            })
+
+            if (deleteImage.status === 200) {
+                const response = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/api/2024-01/products/${productid}/images.json`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Shopify-Access-Token': `${accessToken.access_token}`
+                    },
+                    body: JSON.stringify({ image })
+                })
+
+                const data = await response.json();
+
+                await db.backupimage.create({
+                    data: {
+                        restoreId: `${data.image.id}`,
+                        url: base64ImageForBackup
+                    }
+                })
+
+                await db.backupfilename.create({
+                    data: {
+                        restoreId: `${data.image.id}`,
+                        name: `${singleImageData.name}`
+                    }
+                })
+
+                await db.backupaltname.create({
+                    data: {
+                        restoreId: `${data.image.id}`,
+                        alt: `${singleImageData.name}`
+                    }
+                })
+
+            }
+
+
+        } else {
+            const base64Image2 = Buffer.from(compressedBuffer);
+
+            const getUploadcareImageStatus = await fetch(`https://api.uploadcare.com/files/${id}/storage/`, {
+                headers: {
+                    'Authorization': `Uploadcare.Simple ${process.env.UPLOADCARE_PUBLIC_KEY}:${process.env.UPLOADCARE_SECRET_KEY}`
                 }
+            })
 
-                const queue = 'compressor_to_uploader';
-                channel.assertQueue(queue, { durable: false });
+            const getUploadcareImageUrl = await fetch(`https://api.uploadcare.com/files/${id}/`, {
+                headers: {
+                    'Authorization': `Uploadcare.Simple ${process.env.UPLOADCARE_PUBLIC_KEY}:${process.env.UPLOADCARE_SECRET_KEY}`
+                }
+            })
 
-                const data = JSON.stringify({ id, productid, compressedBuffer, storeName });
-                console.log('length',Buffer.byteLength(data, 'utf8'))
-                channel.sendToQueue(queue, Buffer.from(data));
-                console.log(" [x] Sent to compressor_to_uploader %s", id);
+            const originalFilePath = await getUploadcareImageUrl.json();
 
-                setTimeout(() => {
-                    connection.close();
-                }, 500);
+            console.log(originalFilePath)
+
+            const response = await axios.get(originalFilePath.original_file_url, { responseType: 'arraybuffer' });
+
+            const buffer = Buffer.from(response.data, 'binary');
+            const bufferString = Buffer.from(buffer).toString('base64');
+            // fs.writeFileSync('hello1.jpg', buffer)
+
+            if (getUploadcareImageStatus.status === 200) {
+                const deleteFileReq = await fetch(`https://api.uploadcare.com/files/${id}/storage/`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Uploadcare.Simple ${process.env.UPLOADCARE_PUBLIC_KEY}:${process.env.UPLOADCARE_SECRET_KEY}`
+                    }
+                })
+
+                const data = await uploadFile(
+                    base64Image2,
+                    {
+                        publicKey: 'c0bc9dbd97f5de75c062',
+                        store: 'auto',
+                        fileName: `${singleImageData.name}`,
+                        metadata: {
+                            subsystem: 'js-client',
+                            pet: `COMPRESSED_${id}`
+                        }
+                    }
+                )
+                await db.backupimage.create({
+                    data: {
+                        restoreId: `${data.uuid}`,
+                        url: bufferString
+                    }
+                })
+
+                await db.backupfilename.create({
+                    data: {
+                        restoreId: `${data.uuid}`,
+                        name: `${singleImageData.name}`
+                    }
+                })
+
+                await db.backupaltname.create({
+                    data: {
+                        restoreId: `${data.uuid}`,
+                        alt: `${singleImageData.name}`
+                    }
+                })
+            }
+
+        }
+
+        const existImageFromCustomDB = await fetch(`${process.env.MQSERVER}/image/${id}`)
+
+        if (existImageFromCustomDB.status === 200) {
+            console.log("603", existImageFromCustomDB)
+            const updatedImage = await db.image.update({
+                where: { id: id },
+                data: { status: 'COMPRESSED' },
             });
-        });
+            const removeImageFromCustomDB = await fetch(`${process.env.MQSERVER}/image/${id}`, {
+                method: 'DELETE'
+            })
+        }
 
         res.json({ status: 'Image Uploading started' });
 
